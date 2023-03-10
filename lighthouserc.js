67e7124172f95bc3a61f2117856275191a2b1246
unknown <coryonronda@gmail.com>
2023-03-10 14:35:31 +0900

    no sandbox opt

--- lighthouserc.js
+++ lighthouserc.js
@@ -4,9 +4,9 @@ module.exports = {
         // staticDistDir: "./build",
         url: ["http://localhost:3000/", "http://localhost:3000/second" ],
         startServerCommand: "npm run start", // 서버를 키는 명령어를 통해서도 실행 가능
-        // settings: {
-        //     chromeFlags: "--no-sandbox --headless"
-        // },
+        settings: {
+            chromeFlags: "--no-sandbox --headless"
+        },
         numberOfRuns: 5,
           },
       upload: {
