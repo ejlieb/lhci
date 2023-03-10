66bb6c880f5171b85dfd121f4a59b2d161fe5610
unknown <coryonronda@gmail.com>
2023-03-10 14:27:27 +0900

    fc

---
+++ lighthouserc.js
@@ -0,0 +1,16 @@
+module.exports = {
+    ci: {
+      collect: {
+        // staticDistDir: "./build",
+        url: ["http://localhost:3000/", "http://localhost:3000/second" ],
+        startServerCommand: "npm run start", // 서버를 키는 명령어를 통해서도 실행 가능
+        // settings: {
+        //     chromeFlags: "--no-sandbox --headless"
+        // },
+        numberOfRuns: 5,
+          },
+      upload: {
+        target: 'temporary-public-storage',
+      },
+    },
+  };
