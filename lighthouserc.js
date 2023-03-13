95416cc4be28e3331198b97463df592fcd0fa68a
unknown <coryonronda@gmail.com>
2023-03-13 14:11:01 +0900

    isspa option on

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,12 +1,13 @@
 module.exports = {
     ci: {
       collect: {
-        // staticDistDir: "./build",
-        url: ["http://localhost:3000/", "http://localhost:3000/second" ],
-        startServerCommand: "npm run start", // 서버를 키는 명령어를 통해서도 실행 가능
+        staticDistDir: "./build",
+        // url: ["http://localhost:3000/", "http://localhost:3000/second" ],
+        // startServerCommand: "npm run start", // 서버를 키는 명령어를 통해서도 실행 가능
         settings: {
             chromeFlags: "--no-sandbox --headless"
         },
+        isSinglePageApplication: true,
         numberOfRuns: 5,
           },
       upload: {
