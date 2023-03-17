01a1ccac0952e47f8be3bdd2eb77ad5b888f7704
Minseok Won <coryonronda@gmail.com>
2023-03-17 10:04:27 +0900

    docker network

--- lighthouserc.js
+++ lighthouserc.js
@@ -3,7 +3,7 @@ module.exports = {
       collect: {
         // staticDistDir: "./build",
         url: ["http://localhost:3000/", "http://localhost:3000/second" ],
-        startServerCommand: "npx serve -s build", // 서버를 키는 명령어를 통해서도 실행 가능
+        startServerCommand: "npm run start", // 서버를 키는 명령어를 통해서도 실행 가능
         settings: {
             chromeFlags: "--no-sandbox --headless"
         },
@@ -12,7 +12,7 @@ module.exports = {
       upload: {
         target: 'lhci',
         token:"489fea8f-34f0-411c-8634-ef252884b8f1",
-        serverBaseUrl:"http://172.19.0.2:9001"
+        serverBaseUrl:"http://zealous_khayyam:9001"
       
         // outputDir: './lhci_reports',
         // reportFilenamePattern: '%%PATHNAME%%-%%DATETIME%%-report.%%EXTENSION%%',
