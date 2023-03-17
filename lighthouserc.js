c7eba69e0fb84de59a76e7a36e9ddb1db7e77f6f
Minseok Won <coryonronda@gmail.com>
2023-03-17 09:37:25 +0900

    dd

--- lighthouserc.js
+++ lighthouserc.js
@@ -3,14 +3,19 @@ module.exports = {
       collect: {
         // staticDistDir: "./build",
         url: ["http://localhost:3000/", "http://localhost:3000/second" ],
-        startServerCommand: "npx serve -s build", // 서버를 키는 명령어를 통해서도 실행 가능
+        startServerCommand: "npm run start", // 서버를 키는 명령어를 통해서도 실행 가능
         settings: {
             chromeFlags: "--no-sandbox --headless"
         },
         numberOfRuns: 1,
           },
       upload: {
-        target: 'temporary-public-storage',
+        target: 'lhci',
+        token:"489fea8f-34f0-411c-8634-ef252884b8f1",
+        serverBaseUrl:"http://172.19.0.2:9001"
+      
+        // outputDir: './lhci_reports',
+        // reportFilenamePattern: '%%PATHNAME%%-%%DATETIME%%-report.%%EXTENSION%%',
       },
     },
   };
